f68e06c141f8a4a07bf59671004ef77d1eaf606e
German Arevalo J <germanarevalojerez@gmail.com>
2022-12-12 15:14:13 -0500

    services with dependencies

---
+++ src/app/services/master.service.spec.ts
@@ -0,0 +1,29 @@
+import { MasterService } from './master.service';
+import { FakeValueService } from './value-fake.service';
+import { ValueService } from './value.service';
+
+describe('MasterService', () => {
+  it('should return "my value" from the real service', () => {
+    const valueService = new ValueService();
+    const masterService: MasterService = new MasterService(valueService);
+    expect(masterService.getValue()).toBe('my value');
+  });
+
+  it('should return "other value" from the fake service', () => {
+    const fakeValueService = new FakeValueService();
+    const masterService: MasterService = new MasterService(
+      fakeValueService as unknown as ValueService
+    );
+    expect(masterService.getValue()).toBe('fake value');
+  });
+
+  it('should return "other value" from the fake object', () => {
+    const fake = {
+      getValue: () => 'fake from obj',
+    };
+    const masterService: MasterService = new MasterService(
+      fake as ValueService
+    );
+    expect(masterService.getValue()).toBe('fake from obj');
+  });
+});
