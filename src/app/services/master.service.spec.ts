8f7499c41ba0ed149564b355b5c68e7580196888
German Arevalo J <germanarevalojerez@gmail.com>
2022-12-20 11:40:13 -0500

    TestBed + Spies

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -1,37 +1,58 @@
+import { TestBed } from '@angular/core/testing';
+
 import { MasterService } from './master.service';
 import { FakeValueService } from './value-fake.service';
 import { ValueService } from './value.service';
 
 describe('MasterService', () => {
-  it('should return "my value" from the real service', () => {
-    const valueService = new ValueService();
-    const masterService: MasterService = new MasterService(valueService);
-    expect(masterService.getValue()).toBe('my value');
+  let service: MasterService;
+  let valueServiceSpy: jasmine.SpyObj<ValueService>;
+
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('ValueService', ['getValue']);
+    TestBed.configureTestingModule({
+      providers: [
+        MasterService,
+        { provide: ValueService, useValue: spy },
+      ],
+    });
+    service = TestBed.inject(MasterService);
+    valueServiceSpy = TestBed.inject(
+      ValueService
+    ) as jasmine.SpyObj<ValueService>;
   });
 
-  it('should return "other value" from the fake service', () => {
-    const fakeValueService = new FakeValueService();
-    const masterService: MasterService = new MasterService(
-      fakeValueService as unknown as ValueService
-    );
-    expect(masterService.getValue()).toBe('fake value');
+  it('should be create', () => {
+    expect(service).toBeTruthy();
   });
 
-  it('should return "other value" from the fake object', () => {
-    const fake = {
-      getValue: () => 'fake from obj',
-    };
-    const masterService: MasterService = new MasterService(
-      fake as ValueService
-    );
-    expect(masterService.getValue()).toBe('fake from obj');
-  });
+  // it('should return "my value" from the real service', () => {
+  //   const valueService = new ValueService();
+  //   const masterService: MasterService = new MasterService(valueService);
+  //   expect(masterService.getValue()).toBe('my value');
+  // });
+
+  // it('should return "other value" from the fake service', () => {
+  //   const fakeValueService = new FakeValueService();
+  //   const masterService: MasterService = new MasterService(
+  //     fakeValueService as unknown as ValueService
+  //   );
+  //   expect(masterService.getValue()).toBe('fake value');
+  // });
+
+  // it('should return "other value" from the fake object', () => {
+  //   const fake = {
+  //     getValue: () => 'fake from obj',
+  //   };
+  //   const masterService: MasterService = new MasterService(
+  //     fake as ValueService
+  //   );
+  //   expect(masterService.getValue()).toBe('fake from obj');
+  // });
 
   it('should call getValue from value service', () => {
-    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
     valueServiceSpy.getValue.and.returnValue('fake value');
-    const masterService: MasterService = new MasterService(valueServiceSpy);
-    expect(masterService.getValue()).toBe('fake value');
+    expect(service.getValue()).toBe('fake value');
     expect(valueServiceSpy.getValue).toHaveBeenCalled();
     expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
   });
