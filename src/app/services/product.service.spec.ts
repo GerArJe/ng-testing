d684a3827503ead1da385ea0bd1100939cfdea85
German Arevalo J <germanarevalojerez@gmail.com>
2022-12-26 14:34:36 -0500

    test get

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -7,7 +7,10 @@ import {
 import { ProductsService } from './product.service';
 import { Product } from '../models/product.model';
 import { environment } from './../../environments/environment';
-import { generateManyProducts, generateOneProduct } from '../models/product.mock';
+import {
+  generateManyProducts,
+  generateOneProduct,
+} from '../models/product.mock';
 
 fdescribe('ProductsService', () => {
   let productsService: ProductsService;
@@ -33,7 +36,7 @@ fdescribe('ProductsService', () => {
 
       productsService.getAllSimple().subscribe((data) => {
         expect(data.length).toEqual(mockData.length);
-        expect(data).toEqual(mockData);
+        expect(data.length).toBe(mockData.length);
         doneFn();
       });
 
@@ -41,6 +44,67 @@ fdescribe('ProductsService', () => {
       const url = `${environment.API_URL}/api/v1/products`;
       const req = httpTestingController.expectOne(url);
       req.flush(mockData);
+      httpTestingController.verify();
+    });
+  });
+
+  describe('Tests for getAll', () => {
+    it('should return a product list', (doneFn) => {
+      const mockData: Product[] = generateManyProducts(3);
+
+      productsService.getAll().subscribe((data) => {
+        expect(data.length).toEqual(mockData.length);
+        doneFn();
+      });
+
+      //   http config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(mockData);
+      httpTestingController.verify();
+    });
+
+    it('should return product list with taxes', (doneFn) => {
+      const mockData: Product[] = [
+        { ...generateOneProduct(), price: 100 },
+        { ...generateOneProduct(), price: 200 },
+        { ...generateOneProduct(), price: 0 },
+        { ...generateOneProduct(), price: -100 },
+      ];
+
+      productsService.getAll().subscribe((data) => {
+        expect(data.length).toEqual(mockData.length);
+        expect(data[0].taxes).toEqual(19);
+        expect(data[1].taxes).toEqual(38);
+        expect(data[2].taxes).toEqual(0);
+        expect(data[3].taxes).toEqual(0);
+        doneFn();
+      });
+
+      //   http config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(mockData);
+      httpTestingController.verify();
+    });
+
+    it('should send query params with limit 10 and offset 3', (doneFn) => {
+      const mockData: Product[] = generateManyProducts(3);
+      const limit = 10;
+      const offset = 3;
+
+      productsService.getAll(limit, offset).subscribe((data) => {
+        expect(data.length).toEqual(mockData.length);
+        doneFn();
+      });
+
+      //   http config
+      const url = `${environment.API_URL}/api/v1/products?limit=${limit}&offset=${offset}`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(mockData);
+      const params = req.request.params;
+      expect(params.get('limit')).toEqual(`${limit}`);
+      expect(params.get('offset')).toEqual(`${offset}`);
       httpTestingController.verify();
     });
   });
