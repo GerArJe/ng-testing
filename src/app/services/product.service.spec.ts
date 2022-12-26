ba77b816c8cb3a832b8f474d30c6e2df57055438
German Arevalo J <germanarevalojerez@gmail.com>
2022-12-26 14:06:58 -0500

    HttpClientTestingModule

---
+++ src/app/services/product.service.spec.ts
@@ -0,0 +1,58 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { ProductsService } from './product.service';
+import { Product } from '../models/product.model';
+import { environment } from './../../environments/environment';
+
+fdescribe('ProductsService', () => {
+  let productsService: ProductsService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductsService],
+    });
+
+    productsService = TestBed.inject(ProductsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be create', () => {
+    expect(productsService).toBeTruthy();
+  });
+
+  describe('Tests for getAllSimple', () => {
+    it('should return a product list', (doneFn) => {
+      const mockData: Product[] = [
+        {
+          id: '123',
+          title: 'title',
+          price: 12,
+          description: 'description',
+          category: {
+            id: 122,
+            name: 'name',
+          },
+          images: ['img', 'img'],
+        },
+      ];
+
+      productsService.getAllSimple().subscribe((data) => {
+        expect(data.length).toEqual(mockData.length);
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+
+      //   http config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpTestingController.expectOne(url);
+      req.flush(mockData);
+      httpTestingController.verify();
+    });
+  });
+});
